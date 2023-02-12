f2539e496d6f782f13a6c2b885f61bbe71058366
baozouai <baozouai@gmail.com>
2023-02-12 17:26:25 +0800

    feat: dumi-demo

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,11 +2,9 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
-  base: '/dumi-demo',
+  base: '/dumi-demo/',
   publicPath: '/dumi-demo/',
-
   themeConfig: {
     name: 'dumi-demo',
   },
-  
 });
